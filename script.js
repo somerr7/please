f70421f4e9b6df5f145daf7d627477e58660faaf
somerr7 <somerr7@gmail.com>
2026-03-06 21:20:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const gifStages = [
     "https://media.tenor.com/pjBtpAFSnmgAAAAM/peach-goma-peach-and-goma.gif",  // 1 confused
     "https://media.tenor.com/925LDfyVUGEAAAAm/cute-sad.webp",             // 2 pleading
     "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
+    "https://media.tenor.com/9D8lslRwaeYAAAAj/sad-sorry.gif",       // 4 sadder
     "https://media.tenor.com/9D8lslRwaeYAAAAj/sad-sorry.gif",             // 5 devastated
     "https://media.tenor.com/KkDb5-sgVZkAAAAi/sad-anxious.gif",               // 6 very devastated
     "https://tenor.com/en-GB/view/sad-anxious-cute-bunny-worried-gif-16623750"  // 7 crying runaway
